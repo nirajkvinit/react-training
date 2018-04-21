31a6212b8a3c280c5dfc9f348249d631575eb2f5
Niraj Kumar <nirajkvinit@gmail.com>
2018-04-21 18:58:30 +0530

    ch 2.5

--- reactup1/src/App.js
+++ reactup1/src/App.js
@@ -2,33 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'Tell me the truth without words',
-    url: 'http://kaloraat.com',
-    author: 'Ryan',
-    num_comments: 100,
-    points: 50,
-    objectID: 1
-  },
-  {
-    title: 'Oh no! The candle is out',
-    url: 'http://kaloraat.com',
-    author: 'Zen',
-    num_comments: 50,
-    points: 20,
-    objectID: 2
-  },
-  {
-    title: 'The hard earned black belt',
-    url: 'http://kaloraat.com',
-    author: 'Ninja',
-    num_comments: 10,
-    points: 5,
-    objectID: 3
-  }
-]
-
+import list from './list'
 
 class App extends Component {
   render() {
@@ -37,7 +11,7 @@ class App extends Component {
         {
           list.map(function(item){
             return (
-              <div>
+              <div key={item.objectID}>
                 <h1><a href={item.url}>{item.title}</a> by {item.author}</h1>
                 <h4>{item.num_comments} Comments | {item.points} Points</h4>
               </div>
