982590ed098fa55356244e91ab28b660e41abfeb
Niraj Kumar <nirajkvinit@gmail.com>
2018-04-21 18:53:16 +0530

    ch 2.4

--- reactup1/src/App.js
+++ reactup1/src/App.js
@@ -2,13 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let paragraph = "This is a sample paragraph.";
+const list = [
+  {
+    title: 'Tell me the truth without words',
+    url: 'http://kaloraat.com',
+    author: 'Ryan',
+    num_comments: 100,
+    points: 50,
+    objectID: 1
+  },
+  {
+    title: 'Oh no! The candle is out',
+    url: 'http://kaloraat.com',
+    author: 'Zen',
+    num_comments: 50,
+    points: 20,
+    objectID: 2
+  },
+  {
+    title: 'The hard earned black belt',
+    url: 'http://kaloraat.com',
+    author: 'Ninja',
+    num_comments: 10,
+    points: 5,
+    objectID: 3
+  }
+]
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{ paragraph }</h1>
+        {
+          list.map(function(item){
+            return (
+              <div>
+                <h1><a href={item.url}>{item.title}</a> by {item.author}</h1>
+                <h4>{item.num_comments} Comments | {item.points} Points</h4>
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
--- reactup1/src/index.js
+++ reactup1/src/index.js
@@ -4,22 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//let index_para = "This is a paragraph from index.js";
-const index_para = {
-    text: "This is a paragraph from index.js"
-}
-
-class Hello extends React.Component {
-    render() {
-        index_para.new_para = "Another paragraph in index.js"
-        return (
-            <div>
-                <h2>{index_para.text}</h2>
-                <p>{index_para.new_para}</p>
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
